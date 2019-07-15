c7ab055b85a0802e3fd31e84f0f81805dc63c1a2
andela-pijege <precious.ijege@andela.com>
2019-07-15 15:07:29 +0100

    add url for signup

--- flight_backend/api/urls.py
+++ flight_backend/api/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
-from . import views
+from rest_framework.routers import SimpleRouter
 
+
+from .views import UsersViewSet
+
+router = SimpleRouter()
+
+router.register("users", UsersViewSet, "users")
 urlpatterns = []
+
+urlpatterns += router.urls
