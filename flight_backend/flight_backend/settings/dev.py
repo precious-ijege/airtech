fc52652c862fa3b743288ac8e976feaaf892b600
andela-pijege <precious.ijege@andela.com>
2019-07-19 12:43:30 +0100

    add jwt settings

--- flight_backend/flight_backend/settings/dev.py
+++ flight_backend/flight_backend/settings/dev.py
@@ -5,14 +5,14 @@ from .base import *  # noqa: F403,F401
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS += ['localhost', '127.0.0.1']  # noqa ignore=F405
+ALLOWED_HOSTS += ["localhost", "127.0.0.1"]  # noqa ignore=F405
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DB_NAME"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
     }
 }
