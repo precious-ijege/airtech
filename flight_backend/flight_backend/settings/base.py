1a8456f2432384f8dd1032c04e98d542085c50e9
andela-pijege <precious.ijege@andela.com>
2019-07-26 15:20:13 +0100

    update token setting, and add new url

--- flight_backend/flight_backend/settings/base.py
+++ flight_backend/flight_backend/settings/base.py
@@ -109,12 +109,12 @@ JWT_AUTH = {
     "JWT_VERIFY": True,
     "JWT_VERIFY_EXPIRATION": True,
     "JWT_LEEWAY": 0,
-    "JWT_EXPIRATION_DELTA": datetime.timedelta(seconds=300),
+    "JWT_EXPIRATION_DELTA": datetime.timedelta(seconds=10000),
     "JWT_AUDIENCE": None,
     "JWT_ISSUER": None,
     "JWT_ALLOW_REFRESH": False,
     "JWT_REFRESH_EXPIRATION_DELTA": datetime.timedelta(days=7),
-    "JWT_AUTH_HEADER_PREFIX": "Bearer",
+    "JWT_AUTH_HEADER_PREFIX": "Token",
     "JWT_AUTH_COOKIE": None,
 }
 
