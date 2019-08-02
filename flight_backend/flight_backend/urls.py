638d6bde77cebf81f182a558f90e116092915337
andela-pijege <precious.ijege@andela.com>
2019-08-02 15:30:41 +0100

    include settings for media root and media url

--- flight_backend/flight_backend/urls.py
+++ flight_backend/flight_backend/urls.py
@@ -16,4 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("api/v1/", include("api.urls")), path("admin/", admin.site.urls)]
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("api/v1/", include("api.urls")),
+    path("admin/", admin.site.urls)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
