7098ca325b70f4823efe3a3ef10a2aab4a7ff4f7
andela-pijege <precious.ijege@andela.com>
2019-09-11 22:12:45 +0100

    refactor file for production

--- flight_backend/manage.py
+++ flight_backend/manage.py
@@ -2,9 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
 
-load_dotenv()
 
 def main():
     if os.environ["ENV"] == "PRODUCTION":
@@ -14,15 +12,12 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", setting)
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
 
 
