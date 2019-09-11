765a6d17e03840629b7a53a92c38bb86d5ada758
andela-pijege <precious.ijege@andela.com>
2019-09-11 22:06:08 +0100

    change production settings

--- flight_backend/manage.py
+++ flight_backend/manage.py
@@ -2,7 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def main():
     if os.environ["ENV"] == "PRODUCTION":
@@ -12,12 +14,15 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", setting)
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    except ImportError:
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
     execute_from_command_line(sys.argv)
 
 
