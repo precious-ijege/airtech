3066e9e9d4c12949ac1dec60b86da18b3e85ec78
andela-pijege <precious.ijege@andela.com>
2019-09-11 21:54:05 +0100

    change settings

--- flight_backend/flight_backend/settings/prod.py
+++ flight_backend/flight_backend/settings/prod.py
@@ -6,5 +6,5 @@ from .base import *  # noqa: F403,F401
 DEBUG = False
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-DATABASES["default"] = dj_database_url.config(default=os.environ["DATABASE_URL"])
+DATABASES["default"] = dj_database_url.config(default=os.getenv["DATABASE_URL"])
 ALLOWED_HOSTS += []  # noqa ignore=F405
