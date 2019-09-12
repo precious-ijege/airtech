6295cc508290ecfb2b88ea8c03748668af011354
andela-pijege <precious.ijege@andela.com>
2019-09-13 00:21:27 +0100

    add static settings

--- flight_backend/flight_backend/settings/base.py
+++ flight_backend/flight_backend/settings/base.py
@@ -156,7 +156,7 @@ CELERY_TIMEZONE = "Africa/Lagos"
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
