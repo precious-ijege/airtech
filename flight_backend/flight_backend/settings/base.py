8090ea16b9ae64c9560dc45a99a9b9deff4101d0
andela-pijege <precious.ijege@andela.com>
2019-09-13 00:41:40 +0100

    edit static root

--- flight_backend/flight_backend/settings/base.py
+++ flight_backend/flight_backend/settings/base.py
@@ -158,6 +158,10 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
