48617b623ad17a57fa2d58084b8431df4f426cc6
andela-pijege <precious.ijege@andela.com>
2019-09-16 15:51:10 +0100

    static file settings

--- flight_backend/flight_backend/settings/base.py
+++ flight_backend/flight_backend/settings/base.py
@@ -158,6 +158,10 @@ STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
